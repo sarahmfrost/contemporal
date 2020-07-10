28ea3868995965a8e4c99a528d96ef7d1ebe040b
diyahasteer <diya.hasteer@gmail.com>
2020-07-10 11:34:23 -0700

    scraping titles

--- scrape_art.py
+++ scrape_art.py
@@ -1,9 +1,22 @@
 import pandas as pd
+
 import urllib
+import requests
 import shutil
+from bs4 import BeautifulSoup
+
 import time
 import os
 from random import randint 
 
 titles = []
 images = []
+
+r = urllib.request.urlopen('https://americanindian.si.edu/explore/collections').read()
+soup = BeautifulSoup(r)
+
+for i in soup.find_all('div', {'class': "webmedia"}):
+	titles.append(i.p.text) 
+
+print(titles)
+
