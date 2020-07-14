629e3639405f1d9db63d9cab609f51cdd412e3ea
diyahasteer <diya.hasteer@gmail.com>
2020-07-14 11:23:08 -0700

    finished scraper

--- art scraper/image_scrape.py
+++ art scraper/image_scrape.py
@@ -43,4 +43,24 @@ for lin in links2:
     lin = lin[:0] + "https://collections.si.edu/search/" + lin[0:]
     links3.append(lin)
 
-print(links3)
+# print(links3)
+
+images = []
+
+for i in links3:
+    r3 = rq.get(i)
+    soup3 = BeautifulSoup(r3.text, "html.parser")
+    y = soup3.select('img')
+    for image in y:
+        images.append(image)
+
+for index, img in enumerate(images):
+    img_link = img.get('src')
+    title = img.get('alt')
+    if "/" in title:
+        title = title.replace("/", "")
+    print(img_link)
+    if img_link[:1]=="h":
+        img_data = rq.get(img_link).content
+        with open("art_photos/" + title + '.jpg', 'wb+') as f:
+            f.write(img_data)
