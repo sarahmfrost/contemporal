242f34a6754b65234794df30db465a70cef8e769
Sarah <smfrost@ucsc.edu>
2020-07-30 20:16:34 -0700

    reorganize file structure

---
+++ static/js/lightbox.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+    $('.grid').magnificPopup({
+         delegate: 'img', // child items selector, by clicking on it popup will open
+         type: 'inline',
+         midClick: true
+         // other options
+       });
+});
+
+
