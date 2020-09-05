781f166c08ea3f7b4120c9f285a147b3e9899c83
Sarah <smfrost@ucsc.edu>
2020-09-05 19:28:29 -0400

    adding flask/vscode integration

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/templates/index.html')
-def index():
-    return render_template("index.html")
+#@app.route('/templates/index.html')
+#def index():
+#    return render_template("index.html")
 
 
 if __name__=="__main__":
